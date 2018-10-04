92ed0338c5f2497a8c9bb0b1b48af58b7015e303
J.R <juan.delarosa1224@gmail.com>
2018-10-04 10:11:12 -0500

    JS work

---
+++ JS/external.js
@@ -0,0 +1,12 @@
+// "use strict";
+//
+// console.log("Hello from external JavaScript");
+//
+// alert('Welcome to my Website!');
+//
+// var userFavColor = prompt("Please enter your fav color: ");
+
+
+// parseFloat("2");
+
+
